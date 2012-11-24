cb19412ec9f7a7bcdc55096ffaf93b29cdf98c13
Boris Vujicic <turshija@gmail.com>
2012-11-24 01:19:12 +0100

    Added moving qr code

--- assets/js/desktop.js
+++ assets/js/desktop.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
-    var $moveMe = $("#moveMe"),
+    var $qrCode = $("#qrCode"),
+        $loadingDiv = $("#loadingDiv"),
+        $content = $("#content"),
         servername = $("body").data('servername');
 
-    socket = io.connect('http://'+servername+':8080');
+    try {
+        socket = io.connect('http://'+servername+':8080');
+    } catch (e) {
+        $loadingDiv.text('It looks like our server is down at the moment :( Sorry');
+        return false;
+    }
+    $loadingDiv.hide();
+    $content.show();
 
     socket.on('connect', function() {
         socket.emit('initBrowser');
     });
 
     socket.on('sendBrowserId', function(selfBrowserId) {
-        console.log(selfBrowserId);
+        // console.log(selfBrowserId);
         $("#browserId").text(selfBrowserId);
+        $qrCode.show().attr( 'src', $qrCode.data('imgsrc') + selfBrowserId );
     });
 
     socket.on('sendGyro', function(alpha, beta, gamma) {
         console.log( alpha, beta, gamma );
         $("#debug").text( alpha + ' ' + beta + ' ' + gamma);
 
-        $moveMe.css('top', ( Math.abs(gamma)*2)+50).css('left', (beta*5)+300);
+        $qrCode.css('top', ( Math.abs(gamma)*2)+50).css('left', (beta*5)+300);
     });
 
 });
--- index.php
+++ index.php
@@ -1,26 +1,44 @@
 <?php
-$url = 'http://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']);
+$url = 'http://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']) . "/phone.php?id=";
+$qrUrl = 'http://chart.apis.google.com/chart?cht=qr&chs=150x150&chld=H|0&chl='.$url;
 ?>
 <html>
 <head>
     <title>Test</title>
     <style type="text/css">
-    #moveMe {
+    #qrCode {
+        width:150px;
+        height:150px;
+        border:1px solid #000;
+        position:relative;
+        left:200px;
+        top:50px;
+
+        display: block;
+
+        /* CSS3 movement smoothing, so that movement doesn't get too "jumpy" */
         transition: top 0.1s, left 0.1s, transform 2s;
         -moz-transition: top 0.1s, left 0.1s, -moz-transform 2s;
         -webkit-transition: top 0.1s, left 0.1s, -webkit-transform 2s;
         -o-transition: top 0.1s, left 0.1s,-o-transform 2s;
     }
+    #content {
+        display: none;
+    }
     </style>
 </head>
 <body data-servername="<?php echo $_SERVER['SERVER_NAME'] ?>">
-    <h1>Node test :)</h1>
+    <h1>Phone controller test</h1>
 
-    Open this on phone: <?php echo $url ?>/phone.php?id=<span id="browserId"></span>
+    <div id="loadingDiv">Loading...</div>
 
-    <div id="debug"></div>
+    <div id="content">
+        Open this on phone: <?php echo $url ?><span id="browserId"></span>
 
-    <div id="moveMe" style="width:100px;height:100px;border:1px solid #000;left:200px;top:50px;position:relative">&nbsp;</div>
+        <div id="debug"></div>
+
+        <img src="" id="qrCode" data-imgsrc="<?php echo $qrUrl ?>" />
+    </div>
 
     <script src="http://static2.gametracker.rs/javascript/vendor/jquery.min.js"></script>
     <script src="http://<?php echo $_SERVER['SERVER_NAME'] ?>:8080/socket.io/socket.io.js"></script>
